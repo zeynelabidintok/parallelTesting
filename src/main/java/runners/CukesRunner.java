2ba3e0f888d085f2518ab13b7dc3e01e2abc4546
zeynel abidin <z.abidintok@gmail.com>
2020-01-30 16:25:41 +0100

    Runner class

--- src/main/java/runners/CukesRunner.java
+++ src/main/java/runners/CukesRunner.java
@@ -1,5 +1,14 @@
 package runners;
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
 
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "step_definitions",
+        features = "src/main/resources/features"
+)
 public class CukesRunner {
 
 }
