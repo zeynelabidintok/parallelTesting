5ac9ebf799115f23f75c4d17f5504a713dc641c0
zeynel abidin <z.abidintok@gmail.com>
2020-01-30 16:01:51 +0100

    Added driver class

--- src/main/java/utilities/Driver.java
+++ src/main/java/utilities/Driver.java
@@ -1,7 +1,9 @@
 package utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Driver {
 
@@ -13,6 +15,20 @@ public class Driver {
 
     public WebDriver getDriver(){
         if (driver==null){
+            String browser =ConfigurationReader.getProperty("browser");
+            switch (browser){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver=new ChromeDriver();
+                    break;
+
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver=new FirefoxDriver();
+                    break;
+                default:
+                    throw new RuntimeException("Wrong browser name !");
+            }
             driver=new ChromeDriver();
         }
         return driver;
