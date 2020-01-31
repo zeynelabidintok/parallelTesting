d0abe394a3f82238d1057e932c53f57118a4eef3
zeynel abidin <z.abidintok@gmail.com>
2020-01-31 14:36:37 +0100

    added html report in cukesRunner- added html  report plugn in  pom.xml file

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -9,7 +9,8 @@ import io.cucumber.junit.CucumberOptions;
         glue = "step_definitions",
         features = "src/main/resources/features",
         tags="@view_orders",
-        dryRun = false
+        dryRun = false,
+        plugin = "json:target/cucumber2.json"
 )
 public class CukesRunner {
 
